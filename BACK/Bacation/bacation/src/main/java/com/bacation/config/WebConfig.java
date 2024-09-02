ac1ee0530763cea637d41edc3fb2249f18520403
Taehan Kim <162579158+Taehankk@users.noreply.github.com>
2024-09-02 14:00:40 +0900

    Update WebConfig.java

--- BACK/Bacation/bacation/src/main/java/com/bacation/config/WebConfig.java
+++ BACK/Bacation/bacation/src/main/java/com/bacation/config/WebConfig.java
@@ -11,20 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://i11b307.p.ssafy.io",
                         "http://localhost:5173",
-                        "https://bacation.s3.ap-northeast-2.amazonaws.com",
-                        "https://i11b307.p.ssafy.io:8443"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 
-    // @Override
-    // public void addInterceptors(InterceptorRegistry registry) {
-    //     registry.addInterceptor(new SessionInterceptor())
-    //             .addPathPatterns("/**")
-    //             .excludePathPatterns("/public/**");
-    // }
+    
 }

79bdf3feb449eb3654e4bb23f208cdaa751e1603
Taehan Kim <162579158+Taehankk@users.noreply.github.com>
2024-09-02 13:59:22 +0900

    Update firebase-messaging-sw.js

--- FRONT/Bacation-front/public/firebase-messaging-sw.js
+++ FRONT/Bacation-front/public/firebase-messaging-sw.js
@@ -5,15 +5,6 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.4.0/firebase-messaging-compat.js"
 );
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyDHAnluW822aeAOU1QxL6J07WrfPhPL19U",
-  authDomain: "bacation-23464.firebaseapp.com",
-  projectId: "bacation-23464",
-  storageBucket: "bacation-23464.appspot.com",
-  messagingSenderId: "567893168351",
-  appId: "1:567893168351:web:8497f1e6959679eab0996a",
-  measurementId: "G-E6CNJFB76L"
-});
 
 const messaging = firebase.messaging();
 
